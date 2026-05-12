339a116254fd544e6e3e78fd253345cd0359a423
Polewique <maeva.bachelard.savoie@gmail.com>
2026-05-12 10:30:56 +0200

    baseroute

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ultraboucledetalant">
       <App />
     </BrowserRouter>
   </React.StrictMode>
